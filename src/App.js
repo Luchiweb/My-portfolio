b06e792908261a0050d05f42c659338e6f618209
Svetlana <svetlana.moiseienko.web@gmail.com>
2023-03-03 15:09:40 +0100

    Website navigation added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import "./styles/main.css";
 import { Footer } from "./components/footer/Footer";
 import { Navbar } from "./components/navbar/Navbar";
 import { Home } from "./pages/home/Home";
 import { Projects } from "./pages/projects/Projects";
-import "./styles/main.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Project } from "./pages/project/Project";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      {/* <Home /> */}
-      <Projects />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project" element={<Project />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import sun from "../../img/icons/sun.svg";
 import moon from "../../img/icons/moon.svg";
+
+import { NavLink } from "react-router-dom";
+
 import "./Navbar.css";
 
 export function Navbar() {
@@ -8,9 +11,9 @@ export function Navbar() {
     <nav className="nav">
       <div className="container">
         <div className="nav-row">
-          <a href="./index.html" className="logo">
+          <NavLink to="/" className="logo">
             <strong>Inspire</strong> portfolio
-          </a>
+          </NavLink>
 
           <button className="dark-mode-btn">
             <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />
@@ -19,22 +22,19 @@ export function Navbar() {
 
           <ul className="nav-list">
             <li className="nav-list__item">
-              <a
-                href="./index.html"
-                className="nav-list__link nav-list__link--active"
-              >
+              <NavLink to="/" className="nav-list__link nav-list__link--active">
                 Home
-              </a>
+              </NavLink>
             </li>
             <li className="nav-list__item">
-              <a href="./projects.html" className="nav-list__link">
+              <NavLink to="/projects" className="nav-list__link">
                 Projects
-              </a>
+              </NavLink>
             </li>
             <li className="nav-list__item">
-              <a href="./contacts.html" className="nav-list__link">
+              <NavLink to="/contacts" className="nav-list__link">
                 Contacts
-              </a>
+              </NavLink>
             </li>
           </ul>
         </div>
