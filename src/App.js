f7942e6d6be4ebbe0d4f1e183dd38deb71721c79
Svetlana <svetlana.moiseienko.web@gmail.com>
2023-03-04 10:08:31 +0100

    Create new page: Contacts

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Projects } from "./pages/projects/Projects";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Project } from "./pages/project/Project";
 import ScrollToTop from "./utils/scrollToTop";
+import { Contacts } from "./pages/contacts/Contacts";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:id" element={<Project />} />
+          <Route path="/contacts" element={<Contacts />} />
         </Routes>
         <Footer />
       </div>
