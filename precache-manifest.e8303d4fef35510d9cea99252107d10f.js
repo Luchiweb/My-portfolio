8dc8268729ff1262dfbaf859be13ba996da4b4d3
Svetlana <svetlana.moiseienko.web@gmail.com>
2023-03-05 15:51:01 +0100

    Updates

---
+++ precache-manifest.e8303d4fef35510d9cea99252107d10f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe920b742bbcadd37f8c",
+    "url": "/My-portfolio/static/css/main.c222d2d9.chunk.css"
+  },
+  {
+    "revision": "fe920b742bbcadd37f8c",
+    "url": "/My-portfolio/static/js/main.fe920b74.chunk.js"
+  },
+  {
+    "revision": "e5846861b89810b99e9d",
+    "url": "/My-portfolio/static/js/1.e5846861.chunk.js"
+  },
+  {
+    "revision": "22dae0a92603c7d72c82",
+    "url": "/My-portfolio/static/js/2.22dae0a9.chunk.js"
+  },
+  {
+    "revision": "8a31032e06de5e505d34",
+    "url": "/My-portfolio/static/js/runtime~main.8a31032e.js"
+  },
+  {
+    "revision": "e3a2213cdc9dec577ee0ffc99bb2ea6e",
+    "url": "/My-portfolio/static/media/twitter.e3a2213c.svg"
+  },
+  {
+    "revision": "c5787617d399bcbf94789945a15f86b3",
+    "url": "/My-portfolio/static/media/gitHub.c5787617.svg"
+  },
+  {
+    "revision": "a78d33899d7ec16a5602bef270c93c05",
+    "url": "/My-portfolio/static/media/linkedIn.a78d3389.svg"
+  },
+  {
+    "revision": "ccaa7d239372739f1894437fc06843a9",
+    "url": "/My-portfolio/static/media/sun.ccaa7d23.svg"
+  },
+  {
+    "revision": "c0e1ca3d6c5df5bd7dbad48c0f1d5cb2",
+    "url": "/My-portfolio/static/media/moon.c0e1ca3d.svg"
+  },
+  {
+    "revision": "f04fd5f5c5506e754a54bb54450e65c8",
+    "url": "/My-portfolio/static/media/01.f04fd5f5.jpg"
+  },
+  {
+    "revision": "5bdbd2b2ac15a09807cf0f2174679dd1",
+    "url": "/My-portfolio/static/media/01-big.5bdbd2b2.jpg"
+  },
+  {
+    "revision": "6d43dc6dc26197938e8e9e909effbe22",
+    "url": "/My-portfolio/static/media/02.6d43dc6d.jpg"
+  },
+  {
+    "revision": "155e48d5796c8284a81a104c16d4a83e",
+    "url": "/My-portfolio/static/media/02-big.155e48d5.jpg"
+  },
+  {
+    "revision": "1777ca23092b5e912bc90c3714a371de",
+    "url": "/My-portfolio/static/media/03.1777ca23.jpg"
+  },
+  {
+    "revision": "5eae353c4cd76670aaa434b44e3989a2",
+    "url": "/My-portfolio/static/media/03-big.5eae353c.jpg"
+  },
+  {
+    "revision": "a7643e6d2552d31222040fc3b408b2e4",
+    "url": "/My-portfolio/static/media/gitHub-black.a7643e6d.svg"
+  },
+  {
+    "revision": "64db8531b2a4a3c66b50b393f36e55b1",
+    "url": "/My-portfolio/static/media/header-bg.64db8531.png"
+  },
+  {
+    "revision": "c49e9014ca70d9afec5fc691158530a7",
+    "url": "/My-portfolio/index.html"
+  }
+];
